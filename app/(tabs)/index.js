44e48a9a808bb6e6df31e37de290a286492d0d19
pratham-jaiswal <prathamj0502@gmail.com>
2024-06-03 19:53:23 +0530

    Add UI for home feed posts

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View } from "react-native";
+import React, { useState } from "react";
+import HomeFeed from "../components/homeFeed";
 
 export default function Home() {
+  const [accountData, setAccountData] = useState({
+    username: "pratham",
+    pfpUri: "https://i.imgur.com/uo09s2U.png",
+    bio: "Hi, I'm Pratham!",
+    followers: ["pratham2", "pratham3"],
+    followerCount: 2,
+    following: ["pratham1", "pratham2", "pratham4"],
+    followingCount: 3,
+    posts: [
+      {
+        id: 1,
+        title: "Post 1",
+        postUri: "https://i.imgur.com/khHJxB7.png",
+        username: "pratham1",
+        pfpUri: "https://i.imgur.com/uo09s2U.png",
+      },
+      {
+        id: 2,
+        title: "Post 2",
+        postUri: "https://i.imgur.com/khHJxB7.png",
+        username: "pratham2",
+        pfpUri: "https://i.imgur.com/uo09s2U.png",
+      },
+    ],
+    postsCount: 2,
+  });
+
   return (
     <View>
-      <Text>Home</Text>
+      <HomeFeed accountData={accountData} setAccountData={setAccountData} />
     </View>
-  )
+  );
 }
