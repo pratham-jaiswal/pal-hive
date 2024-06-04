7c528af45d8c02ec1f5c49be0c2a028c5fba121d
pratham-jaiswal <prathamj0502@gmail.com>
2024-06-04 19:37:54 +0530

    Add like notifications

--- app/(tabs)/notifications.js
+++ app/(tabs)/notifications.js
@@ -1,10 +1,67 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { AccountContext } from "../_layout";
+import { useContext, useEffect, useState } from "react";
+import { FlashList } from "@shopify/flash-list";
+import { router } from "expo-router";
 
 export default function Notifications() {
+  const { accountData } =
+    useContext(AccountContext);
+
+  const [likes, setLikes] = useState([]);
+
+  useEffect(() => {
+    tempLikes = [];
+    accountData.posts.forEach((post) => {
+      post.likedBy.forEach((user) => {
+        tempLikes.push({
+          username: user,
+          postId: post.id,
+          postTitle: post.title,
+        });
+      });
+    });
+
+    tempLikes.reverse();
+
+    setLikes(tempLikes);
+  }, []);
+
   return (
-    <View>
-      <Text>Notifications</Text>
+    <View style={styles.container}>
+      <FlashList
+        data={likes}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.notificationContainer} activeOpacity={0.5} onPress={() => router.push("/account#1")}>
+            <Text style={styles.notification}>
+              <Text style={{ fontStyle: "italic", fontWeight: "bold" }}>
+                @{item.username}
+              </Text>{" "}
+              liked your post "{item.postTitle}"
+            </Text>
+          </TouchableOpacity>
+        )}
+        estimatedItemSize={200}
+      />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 0,
+    height: "100%",
+    marginHorizontal: 15,
+  },
+  notificationContainer: {
+    paddingHorizontal: 7,
+    paddingVertical: 20,
+    borderBottomWidth: 1,
+    borderColor: "#fff9d080",
+  },
+  notification: {
+    fontSize: 16,
+    color: "#FFF9D0",
+  },
+});
